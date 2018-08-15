ec8b656251a8482118bd10e57fb5b34e42090b76
Preston A Arnette <arnettepreston@gmail.com>
2018-08-15 23:35:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,11 +71,10 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (typeof cardNumber == 'number') {
-    cart.splice(0, cart.length)
-    
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   } 
   else {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
+  cart.splice(0, cart.length)
 }
