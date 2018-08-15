8aded65c8c43607473d741b62b73205de4028f65
Preston A Arnette <arnettepreston@gmail.com>
2018-08-15 23:14:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  var price = Math.floor(Math.random() * (100 - 1) + 1);
+  var itemObj = {itemName: item, itemPrice: price} ;
+  cart.push(itemObj);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  const message = [];
+  for (const i = 0; i < cart.length; i++){
+    message.push(`${cart[i].itemName} at $${cart[i].itemPrice},`)
+    if (cart.length > 1) {
+      message.push(`${cart[i + 1].itemName} at $${cart[i + 1].itemPrice},`)
+    return `In your cart, you have ${message[i]} and ${message[i + 1]}.`;
+    } else if (i === 0) {
+      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`  
+    }
+  }
+  return `Your shopping cart is empty.`;
 }
+console.log(getCart())
 
 function total() {
   // write your code here
