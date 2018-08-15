b64afd6a706d454db280c290fe1e19edb535b7c3
Preston A Arnette <arnettepreston@gmail.com>
2018-08-15 23:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`
   } 
   else {
-    return 'Sorry, we don'\t have a credit card on file for you'
+    return 'Sorry, we don\'t have a credit card on file for you'
   }
 }
