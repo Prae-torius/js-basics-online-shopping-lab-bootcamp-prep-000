da42d96b7c5ab6827a4987b56a1fefb6f00ee7c8
Preston A Arnette <arnettepreston@gmail.com>
2018-08-15 23:19:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ function viewCart() {
     message.push(`${cart[i].itemName} at $${cart[i].itemPrice},`)
     
     if (cart.length > 1) {
-      message.push(`${cart[i + 1].itemName} at $${cart[i + 1].itemPrice},`)
+      message.push(`${cart[i + 1].itemName} at $${cart[i + 1].itemPrice}`)
     
     return `In your cart, you have ${message[i]} and ${message[i + 1]}.`;
     } 
     
     else if (cart.length >= 3) {
-      message.push(`${cart[i + 1].itemName} at $${cart[i + 1].itemPrice},`)  
-      message.push(`${cart[i + 2].itemName} at $${cart[i + 2].itemPrice},`)
+      message.push(`${cart[i + 1].itemName} at $${cart[i + 1].itemPrice}`)  
+      message.push(`${cart[i + 2].itemName} at $${cart[i + 2].itemPrice}`)
       
       return `In your cart, you have ${message[i]} ${message[i + 1]} and ${message[i + 2]}.`
     } 
