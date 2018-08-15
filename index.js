905663b8ed581d489b706e7646d3b252778de2f1
Preston A Arnette <arnettepreston@gmail.com>
2018-08-15 23:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,8 +67,11 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
-      cart.splice([i],)
+      cart.splice([i], 1)
     }
+    else {
+      return 'That item is not in your cart.'
+    
   }
 }
 
