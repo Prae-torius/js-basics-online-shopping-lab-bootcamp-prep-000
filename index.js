d0f53563e6e281b0171fbe6388157fac2e8d9784
Preston A Arnette <arnettepreston@gmail.com>
2018-08-15 23:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ function placeOrder(cardNumber) {
   // write your code here
   
   if (typeof cardNumber == 'number') {
-    
+    cart.splice(0, cart.length)
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   } 
   
